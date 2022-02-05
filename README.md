921c079ea90c4baa0b102f928a621cd5239788d5
flavialbraz <78583429+flavialbraz@users.noreply.github.com>
2022-02-05 19:31:39 -0300

    Inicial

---
+++ README.md
@@ -0,0 +1,7 @@
+# Exercício
+Desenvolvido o **design e o código** para uma página de login responsiva e **SIMPLES**. <br>
+Na página é informado ao usuário o login que está cadastrado para que ele possa acessar. <br>
+O método não deve ser **NUNCA utilizado em uma aplicação**, foi realizado apenas para fins de estudo.
+
+- Responsivo
+Confira: (https://flavialbraz.github.io/loginpage/login)
---
+++ script.js
@@ -0,0 +1,49 @@
+ 
+
+let emailUser = document.getElementById("email");
+let passwordUser = document.getElementById("password");
+let emailRegistered = "oi@gmail.com";
+let passwordRegistered = "senha";
+ 
+let alertErrorLogin = document.getElementById("alert-error");
+
+ 
+function Login() {
+        if (passwordUser.value === emailRegistered && emailUser.value === passwordRegistered) {
+            location = "home.html";    
+            return false;  
+        }
+
+        if(passwordUser.value === "" && emailUser === ""){
+            return false;                              
+        }  
+         
+        if(emailUser.value != passwordRegistered || passwordUser.value != emailRegistered){
+            alertErrorLogin.classList.add("boxred")
+            alertErrorLogin.innerHTML = " Login inválido. Confira a sua senha ou e-mail" + 
+            '<button id="fecharmensagem" onclick="closeX();">&times;</button>';
+            return false;
+        }
+
+}
+ 
+function showPassword() {
+        let inputPassword = document.getElementById("password");
+
+        if (inputPassword.type === "password") {
+            inputPassword.type = "text";
+         }
+        
+        else {
+            inputPassword.type = "password";
+        }
+  }
+ 
+ 
+let messageWithLogin = "Utilize nosso login padrão: Email: " + emailRegistered + " | Senha: " + passwordRegistered;
+ 
+function closeX () {
+    alertErrorLogin.classList.remove("boxred");
+}
+
+ 
