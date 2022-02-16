1ef2ebb292975758b38d19a6a453e50291f649bc
Flávia Al. Braz <78583429+flavialbraz@users.noreply.github.com>
2022-02-16 20:29:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ let alertErrorLogin = document.getElementById("alert-error");
 
  
 function Login() {
-        if (passwordUser.value === passwordRegistered && emailUser.value.toLowerCase() === emailRegistered) {
+        if (passwordUser.value === passwordRegistered && emailUser.value === emailRegistered) {
             location = "home.html";    
         }
 
         if(!passwordUser.value && !emailUser){
         }  
          
-        if(passwordUser.value != passwordRegistered || emailUser.value.toLowerCase() != emailRegistered){
+        if(passwordUser.value != passwordRegistered || emailUser.value != emailRegistered){
             alertErrorLogin.classList.add("boxred")
             alertErrorLogin.innerHTML = " Login inválido. Confira a sua senha ou e-mail" + 
             '<button id="closemsg" onclick="closeX();">&times;</button>';
